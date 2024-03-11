cb3ec02ff65f5b27892e82ad35674e07a5e17509
Chandru Angadi <83079815+Chandru779@users.noreply.github.com>
2024-03-12 00:01:04 +0530

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,7 +6,7 @@ import { slideinFromLeft } from '@/utils/motions'
 const Logo = () => {
     const MotionLink = motion(Link)
   return (
-    <div className='flex items-center justify-center'>
+    <div className='flex items-center justify-center -ml-14'>
         {/* <MotionLink href='mailto:chanduangadi779@gmail.com' className='px-2 py-1 bg-dark text-white flex items-center justify-center text-xl rounded-xl font-mono font-thin tracking-wide'  
         whileHover={{
             backgroundColor:["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"],
